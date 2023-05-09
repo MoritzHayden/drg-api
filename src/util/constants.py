c5942e7670f1d3d9884166201496cee2389266b9
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-05-09 15:05:01 -0500

    Remove domain constants

--- src/util/constants.py
+++ src/util/constants.py
@@ -1,17 +1,13 @@
 # API
 API_TITLE: str = "DRG API"
-API_DESCRIPTION: str = "DRG API is a free API which returns the current Deep Dive information for Deep Rock Galactic (updated weekly)."
-API_VERSION: str = "0.1.0"
-API_LICENSE_NAME: str = "MIT"
+API_DESCRIPTION: str = "DRG API is a free API which returns Deep Rock Galactic information and resources."
+API_VERSION: str = "1.0.0"
+API_LICENSE_NAME: str = "MIT License"
+API_ROOT: str = "/"
 
 # Contact
 CONTACT_NAME: str = "DRG API Help"
 CONTACT_EMAIL: str = "help@drgapi.com"
-
-#Domain
-DOMAIN_ROOT_URL: str = "https://drgapi.com"
-DOMAIN_TOS_URL: str = "/".join([DOMAIN_ROOT_URL, "tos"])
-DOMAIN_LICENSE_URL: str = "/".join([DOMAIN_ROOT_URL, "license"])
 
 # GitHub
 GITHUB_ROOT_URL: str = "https://github.com/MoritzHayden/drg-api"
