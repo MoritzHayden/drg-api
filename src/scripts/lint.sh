0b58b9425e4b8b763f292cd0f2fda3af5c4a9c28
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-05-10 15:17:53 -0500

    Add lint script

---
+++ src/scripts/lint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Set install flag to false
+install_flag=false
+
+# Print help message
+print_help() {
+    echo "Run Pylint on all python files."
+    echo " "
+    echo "Usage: ./lint.sh [options]"
+    echo " "
+    echo "Options:"
+    echo "-h        show brief help"
+    echo "-i        intall dependencies"
+    exit 0
+}
+
+# Parse command line arguments
+while getopts 'ih' flag; do
+    case "${flag}" in
+    i) install_flag=true ;;
+    h) print_help ;;
+    *) print_help ;;
+    esac
+done
+
+# Move to src directory
+cd "$(dirname "$(readlink -f "$0")")/.."
+
+# Run the install script with dev dependencies if the flag is set
+if [ "$install_flag" = true ]; then
+    echo "Running install script..."
+    ./scripts/install.sh -d
+fi
+
+# Run Pylint
+echo "Running Pylint..."
+pipenv run pylint --rcfile .pylintrc **/*.py
